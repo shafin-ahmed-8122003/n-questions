54fe8d79d7813efc58b513fc842d6b1aa507fc0a
Shafin <shafin.ahmed.8122003@gmail.com>
2024-06-06 15:32:25 +0600

    Landing page designed with responsiveness

--- src/app/(public)/(landing)/page.tsx
+++ src/app/(public)/(landing)/page.tsx
@@ -5,16 +5,18 @@ import Image from "next/image";
 
 const LandingPage = () => {
     return (
-        <main className="h-full flex flex-col items-center justify-center">
+        <main className="h-full flex flex-col items-center justify-center p-8">
             <div className="flex flex-col justify-center items-center">
                 <Image src={landingLogo} alt="N-Questions Logo" width={400} priority />
-                <p className="font-medium text-xl">Get to know yourself from anonymous persons</p>
+                <p className="font-medium text-xl text-center">
+                    Get to know yourself from anonymous persons
+                </p>
                 <Gap height={30} />
                 <Button size="lg" className="w-[300px]">
                     Get Started
                 </Button>
                 <Gap height={10} />
-                <p className="text-lg">
+                <p className="text-lg text-center">
                     Already have an account?{" "}
                     <Button size="link" variant="link">
                         Click here
