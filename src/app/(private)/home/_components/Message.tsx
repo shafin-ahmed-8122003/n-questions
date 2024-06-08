338cdf22822d57e220d4f1302dcbe7417bf084cf
Shafin <shafin.ahmed.8122003@gmail.com>
2024-06-08 20:09:11 +0600

    Capture image functionality added

--- src/app/(private)/home/_components/Message.tsx
+++ src/app/(private)/home/_components/Message.tsx
@@ -5,30 +5,48 @@ import Gap from "@/src/components/Gap";
 import { Button } from "@/src/components/ui/button";
 import formatTimestamp from "@/src/lib/formatTimestamp";
 import { useMutation } from "convex/react";
-import { Share2Icon, Trash2Icon } from "lucide-react";
+import html2canvas from "html2canvas";
+import { DownloadIcon, Trash2Icon } from "lucide-react";
+import { useRef } from "react";
 import { toast } from "sonner";
 
 const Message = ({ message }: { message: any }) => {
     const deleteMessage = useMutation(api.messages.deleteMessage);
+    const captureRef = useRef(null);
 
     const onDelete = (messageId: any) => {
         deleteMessage({ messageId: messageId });
         toast("Message deleted");
     };
 
+    const handleCaptureClick = async () => {
+        if (captureRef.current) {
+            const canvas = await html2canvas(captureRef.current);
+            const imgData = canvas.toDataURL("image/png");
+            const link = document.createElement("a");
+            link.href = imgData;
+            link.download = "screenshot.png";
+            link.click();
+        }
+    };
+
     return (
-        <div className="w-full min-h-[150px] border-2 border-primary rounded-lg flex flex-col justify-between p-4">
-            <h2 className="lg:text-xl text-lg">{message.question}</h2>
-            <Gap height={10} />
-            <div className="flex items-center justify-between">
-                <time className="lg:text-lg text-sm">{formatTimestamp(message._creationTime)}</time>
-                <div className="flex items-center gap-2">
-                    <Button className="w-12">
-                        <Share2Icon className="stroke-primary-foreground" />
-                    </Button>
-                    <Button onClick={() => onDelete(message._id)} className="w-12">
-                        <Trash2Icon className="stroke-primary-foreground" />
-                    </Button>
+        <div ref={captureRef} className="p-1">
+            <div className="w-full min-h-[150px] border-2 border-primary rounded-lg flex flex-col justify-between p-4">
+                <h2 className="lg:text-xl text-lg">{message.question}</h2>
+                <Gap height={10} />
+                <div className="flex items-center justify-between">
+                    <time className="lg:text-lg text-sm">
+                        {formatTimestamp(message._creationTime)}
+                    </time>
+                    <div className="flex items-center gap-2">
+                        <Button onClick={handleCaptureClick} className="w-12">
+                            <DownloadIcon className="stroke-primary-foreground" />
+                        </Button>
+                        <Button onClick={() => onDelete(message._id)} className="w-12">
+                            <Trash2Icon className="stroke-primary-foreground" />
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
